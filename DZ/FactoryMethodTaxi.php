788fd5faff1e1b58453bf7d67b38998850c30e67
buryk20 <73736907+buryk20@users.noreply.github.com>
2023-05-24 08:57:32 +0300

    taxi

--- DZ/FactoryMethodTaxi.php
+++ DZ/FactoryMethodTaxi.php
@@ -4,9 +4,16 @@ class DeliveryType
 {
     private $car;
 
-    public function __construct(Car $car)
-    {
+    public function __construct(Car $car) {
         $this->car = $car;
+    }
+
+    public function getCarModel() {
+        return $this->car->getModel();
+    }
+
+    public function getCarPrice() {
+        return $this->car->getPrice();
     }
 }
 
@@ -25,70 +32,33 @@ class Car
     }
 }
 
-// class DeliveryType {
-//     private $car;
-
-//     public function __construct(Car $car) {
-//         var_dump($car);
-//         $this->car = $car;
-//     }
-
-//     public function getCarModel() {
-//         return $this->car->getModel();
-//     }
-
-//     public function getCarPrice() {
-//         return $this->car->getPrice();
-//     }
-// }
-
-// class Car {
-//     protected $model;
-//     protected $price;
-
-//     public function getModel() {
-//         return $this->model;
-//     }
-
-//     public function getPrice() {
-//         return $this->price;
-//     }
-// }
-
 class EconomyCar extends Car {
     public function __construct() {
-        var_dump($this);
         $this->model = "Economy Car";
-        $this->price = 10; // Додайте реальні значення ціни
+        $this->price = 10;
     }
 }
 
 class StandardCar extends Car {
     public function __construct() {
         $this->model = "Standard Car";
-        $this->price = 20; // Додайте реальні значення ціни
+        $this->price = 20;
     }
 }
 
 class LuxuryCar extends Car {
     public function __construct() {
         $this->model = "Luxury Car";
-        $this->price = 30; // Додайте реальні значення ціни
+        $this->price = 30;
     }
 }
 
-// Приклад використання
-
-// Створення об'єктів різних типів таксі
 $economyDelivery = new DeliveryType(new EconomyCar());
 $standardDelivery = new DeliveryType(new StandardCar());
 $luxuryDelivery = new DeliveryType(new LuxuryCar());
 
-// var_dump($economyDelivery);
-
-// Виведення моделі та ціни таксі
-// echo $economyDelivery->getCarModel() . ": $" . $economyDelivery->getCarPrice() . PHP_EOL;
-// echo $standardDelivery->getCarModel() . ": $" . $standardDelivery->getCarPrice() . PHP_EOL;
-// echo $luxuryDelivery->getCarModel() . ": $" . $luxuryDelivery->getCarPrice() . PHP_EOL;
+echo $economyDelivery->getCarModel() . ": $" . $economyDelivery->getCarPrice() . PHP_EOL;
+echo $standardDelivery->getCarModel() . ": $" . $standardDelivery->getCarPrice() . PHP_EOL;
+echo $luxuryDelivery->getCarModel() . ": $" . $luxuryDelivery->getCarPrice() . PHP_EOL;
 
 ?>
