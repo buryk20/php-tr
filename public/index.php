cd04ecf0cd61cbc9729bf56c4cc323fc36030649
buryk20 <73736907+buryk20@users.noreply.github.com>
2023-05-31 16:00:22 +0300

    composer

--- public/index.php
+++ public/index.php
@@ -1,14 +1,14 @@
 <?php
 require_once dirname(__DIR__) . '/Config/constants.php';
-// require_once BASE_DIR . '/vendor/autoload.php';
+require_once BASE_DIR . '/vendor/autoload.php';
 
 if (!session_id()) {
     session_start();
 }
 
-// $dotenv = Dotenv\Dotenv::createUnsafeImmutable(__DIR__);
-// $dotenv->load();
+$dotenv = Dotenv\Dotenv::createUnsafeImmutable(__DIR__);
+$dotenv->load();
 // dd($_SERVER);
-var_dump($_SERVER);
+// var_dump($_SERVER);
 
 echo '</br>Hello World';
