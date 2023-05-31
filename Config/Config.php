c14371fc7d8ec700a1eb960dcb2d1dfbfbf74b4c
buryk20 <73736907+buryk20@users.noreply.github.com>
2023-05-31 14:37:53 +0300

    php

--- Config/Config.php
+++ Config/Config.php
@@ -4,4 +4,19 @@ namespace Config;
 
 class Config
 {
+    protected static Config|null $instance = null;
+
+    public static function get(string $name): string|null
+    {
+        if(is_null(static::$instance)) {
+            static::$instance = new static;
+        }
+
+        return static::$instance->getParam($name);
+    }
+
+    public function getParam(string $name): string|null
+    {
+        return null;
+    }
 }
